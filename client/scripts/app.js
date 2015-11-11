7deee5e0600d68c28467305f6272822b56f129e5
pbhorjee <paul@bhorjee.com>
2015-11-10 21:26:03 -0600

    audio player, song conrols

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,11 @@
+SC.initialize({
+  client_id: SOUND_CLOUD_KEY
+});
+
+//TODO: error is remove logic - weird songs being added to currentSong prop
+
 var AppContainer = React.createClass({
   SCrequest: function (searchText) {
-    SC.initialize({
-      client_id: SOUND_CLOUD_KEY
-    });
-
     var component = this;
 
     SC.get('/tracks', {
@@ -15,28 +17,91 @@ var AppContainer = React.createClass({
   },
 
   addToPlaylist: function (song) {
-    var playlist = this.state.songs;
-    playlist.push(song);
+    if (this.state.songs.indexOf(song) === -1) {
+      var playlist = this.state.songs.slice(0);
+      playlist.push(song);
+      this.setState({songs: playlist});
+    }
+  },
 
-    console.log("playlist", playlist);
-
-    this.setState({songs: playlist});
+  playSong: function(song) {
+    var component = this;
+    var track = "/tracks/" + song.id;
+    SC.get(track)
+      .then(function (track) {
+        component.setState({currentSong: track});
+      });
   },
 
   getInitialState: function () {
     return {
       data: [],
-      songs: []
+      songs: [],
+      currentSong: {
+        stream_url: ''
+      }
     };
+  },
+
+  playNextSong: function() {
+    // search playlist for the current song id
+    var songIds = _.pluck(this.state.songs, 'id');
+    var thisIndex = -1;
+    for (var i = 0; i < songIds.length; i++) {
+      if ( songIds[i] === this.state.currentSong.id ) {
+        thisIndex = i;
+      }
+    }
+
+    // set current song to the next song in the playlist
+    var nextIndex = 0;
+    if (thisIndex !== this.state.songs.length - 1) {
+      nextIndex = thisIndex + 1;
+    }
+
+    this.playSong(this.state.songs[nextIndex]);
+  },
+
+  clearPlaylist: function() {
+    this.setState({
+      songs: [],
+      currentSong: {}
+    });
+  },
+
+  removeSong: function(song) {
+    // search playlist for the id of the song that is being removed
+    var songIds = _.pluck(this.state.songs, 'id');
+    var thisIndex = -1;
+    for (var i = 0; i < songIds.length; i++) {
+      if ( songIds[i] === song.id ) {
+        thisIndex = i;
+      }
+    }
+
+    // if song being removed is the currentSong, playNextSong()
+    if (song.id === this.state.currentSong.id) {
+      this.playNextSong();
+    }
+
+    // if only one song left in playlist, clear playlist
+
+    var playlist = this.state.songs.slice(0);
+    playlist.splice(thisIndex,1);
+    this.setState({songs: playlist});
   },
 
   render: function () {
     return (
       <div className="AppContainer">
         <h1>Search</h1>
+        <AudioPlayer currentSong={this.state.currentSong} onSongEnded={this.playNextSong}/>
         <SearchInput onSearchSubmit={this.SCrequest}/>
         <SearchResults data={this.state.data} onSelectClick={this.addToPlaylist}/>
-        <Playlist songs={this.state.songs} />
+        <Playlist songs={this.state.songs}
+                  clearPlaylist={this.clearPlaylist}
+                  onSelectClick={this.playSong}
+                  onRemoveSong={this.removeSong} />
       </div>
     );
   }
@@ -71,7 +136,14 @@ var SearchResults = React.createClass({
 
     var songNodes = this.props.data.map(function (song) {
       return (
-        <Song key={song.id} title={song.title} song={song} onSelectClick={component.handleSelect}></Song>
+        <Song
+          key={song.id} 
+          title={song.title} 
+          song={song} 
+          onSelectClick={component.handleSelect}
+          onRemoveSong={component.removeSong}
+          showRemoveButton={false}
+        ></Song>
       );
     });
     return (
@@ -90,11 +162,22 @@ var Song = React.createClass({
     this.props.onSelectClick(this.props.song);
   },
 
+  removeSong: function() {
+    this.props.onRemoveSong(this.props.song);
+  },
+
   render: function () {
+    var removeButton;
+
+    if (Boolean(this.props.showRemoveButton)) {
+      removeButton = <button onClick={this.removeSong}>x</button>;
+    }
+
     return (
       <div className='song'>
         <h4 className='songTitle' onClick={this.handleSelect}>
           {this.props.title}
+          { removeButton }
         </h4>
       </div>
     );
@@ -102,21 +185,32 @@ var Song = React.createClass({
 });
 
 var Playlist = React.createClass({
-  handleSelect: function () {
-    this.props.onSelectClick(); //handle play here
+  handleSelect: function (song) {
+    this.props.onSelectClick(song);
   },
+
+  removeSong: function(song) {
+  this.props.onRemoveSong(song);
+},
 
   render: function () {
     var component = this;
 
     var songNodes = this.props.songs.map(function (song) {
       return (
-        <Song key={song.id} title={song.title} song={song} onSelectClick={component.handleSelect}></Song>
+        <Song key={song.id}
+              title={song.title}
+              song={song}
+              onRemoveSong={component.removeSong}
+              onSelectClick={component.handleSelect}
+              showRemoveButton={true} >
+        </Song>
       );
     });
     return (
       <div>
         <h2>Playlist</h2>
+        <button type='button' onClick={component.props.clearPlaylist}>Clear Playlist</button>
         <div>
           <div className='searchResults'>
             {songNodes}
@@ -127,10 +221,21 @@ var Playlist = React.createClass({
   }
 });
 
+var AudioPlayer = React.createClass({
+  render: function() {
+    var srcString = '';
+    if (this.props.currentSong.stream_url) {
+      srcString = this.props.currentSong.stream_url.replace('http:','https:') + '?client_id=' + SOUND_CLOUD_KEY;
+    }
+
+    return (
+      <audio controls autoPlay src={srcString} onEnded={this.props.onSongEnded} ref='audioPlayer'></audio>
+    );
+  }
+});
+
 ReactDOM.render(
   <AppContainer />,
   document.getElementById('content')
 );
 
-// anytime we change state, render should get called automatically
-// setState vs. updateState
